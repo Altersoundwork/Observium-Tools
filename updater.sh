b7d36d20a565fac3dda62c03bf980cf9197fd871
Luciel Campbell <luciel@neonit.net>
2021-04-13 13:36:52 +0200

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -10,6 +10,25 @@ echo "Observium Update"
 echo "################"
 echo ${normal}
 #######################
+date=$(date +"%d-%m-%Y@%H-%M")
+#
+## First we will make a DB and File backup, change the following acccordingly if it does not match your setup.
+## This will be stored in your home folder.
+#
+cd $HOME
+mysqldump -u observium observium > observium_db_backup.sql
+sudo tar cvf server-ob_db_bkup_$date.tar.gz observium_db_backup.sql
+sudo tar cvf server-ob_files_bkup_$date.tar.gz /opt/observium
+sudo mkdir Observium_BKUP_$date
+mv server-ob_db_bkup_$date.tar.gz Observium_BKUP_$date && mv server-ob_files_bkup_$date.tar.gz Observium_BKUP_$date
+sudo tar cvf Observium_BKUP_$date.tar.gz Observium_BKUP_$date
+#
+## Cleanup
+#
+sudo rm -rf observium_db_backup.sql Observium_BKUP_$date/
+#
+## Update while keeping Logs, RRD and config.ph
+#
 cd /opt
 sudo mv observium observium_old
 sudo wget -Oobservium-community-latest.tar.gz https://www.observium.org/observium-community-latest.tar.gz
