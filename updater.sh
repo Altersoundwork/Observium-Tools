ded545248a51dd49c9235af66af1aee2ce3309eb
Luciel Campbell <luciel@neonit.net>
2021-07-29 14:35:13 +0200

    Added extra backup (.tar.gz)
    Added extra backup (.tar.gz) prior to update in case of shenanigans when removed the old folder after the fact. Cleaned it up a bit too.

--- updater.sh
+++ updater.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #
-# date: 13/04/2021
+# Updates any observium install to the latest version while backing up for security.
+# v0.2 - 29-07-2021
 # dev: @Altersoundwork
 #
+clear
 #######################
 echo ${bold}
 echo "################"
@@ -10,32 +12,14 @@ echo "Observium Update"
 echo "################"
 echo ${normal}
 #######################
-date=$(date +"%d-%m-%Y@%H-%M")
-#
-## First we will make a DB and File backup, change the following acccordingly if it does not match your setup.
-## This will be stored in your home folder.
-#
-cd $HOME
-mysqldump -u observium observium > observium_db_backup.sql
-sudo tar cvf server-ob_db_bkup_$date.tar.gz observium_db_backup.sql
-sudo tar cvf server-ob_files_bkup_$date.tar.gz /opt/observium
-sudo mkdir Observium_BKUP_$date
-mv server-ob_db_bkup_$date.tar.gz Observium_BKUP_$date && mv server-ob_files_bkup_$date.tar.gz Observium_BKUP_$date
-sudo tar cvf Observium_BKUP_$date.tar.gz Observium_BKUP_$date
-#
-## Cleanup
-#
-sudo rm -rf observium_db_backup.sql Observium_BKUP_$date/
-#
-## Update while keeping Logs, RRD and config.ph
-#
 cd /opt
-sudo mv observium observium_old
+sudo tar cvf observium_pre_update.tar.gz observium/
+sudo mv observium observium_pre_update
 sudo wget -Oobservium-community-latest.tar.gz https://www.observium.org/observium-community-latest.tar.gz
 sudo tar zxvf observium-community-latest.tar.gz
-sudo cp /opt/observium_old/rrd observium/
-sudo cp /opt/observium_old/logs observium/
-sudo cp /opt/observium_old/config.php observium/
+sudo cp /opt/observium_pre_update/rrd observium/
+sudo cp /opt/observium_pre_update/logs observium/
+sudo cp /opt/observium_pre_update/config.php observium/
 echo
 read -p "${bold}If you see no errors, we're now ready to perform the update. Do you wish to continue?${normal}" -n 1 -r
 echo
