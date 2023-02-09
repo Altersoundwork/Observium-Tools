9737e3b64b8f36b416f605c890ead7082b875b2e
Luciel Campbell <luciel@neonit.net>
2023-02-09 14:16:37 +0100

    Update updater.sh
    Fixed polling issue due to permissions (look at Notes within the script).

--- updater.sh
+++ updater.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Updates any observium install to the latest version while backing up for security.
-# v0.2 - 27-06-2022
+# v0.4 - 09-02-2023
 # dev: @Altersoundwork
 #
 clear
@@ -12,15 +12,19 @@ echo "Observium Update"
 echo "################"
 echo ${normal}
 #######################
+# Notes:
+# Observium is very specific about permissions of individual files and folders, because of this, we not only make a backup file but also move the original folder
+# so we can copy the pertinent folders and files after the update (which is esentially a reinstall aniway) mantaining the correct permisions. Not doing this causes
+# polling to break and Observium needing a clean RRD and Logs folder to start polling again.
+#######################
 cd /opt
-sudo tar cvf observium_pre_update_$date.tar.gz observium/
+sudo tar cvf observium_pre_update_bkup_$date.tar.gz observium/
 sudo mv observium observium_pre_update_$date
 sudo wget -Oobservium-community-latest.tar.gz https://www.observium.org/observium-community-latest.tar.gz
 sudo tar zxvf observium-community-latest.tar.gz
-sudo cp -r /opt/observium_pre_update_$date/rrd /opt/observium/
-sudo cp -r /opt/observium_pre_update_$date/logs /opt/observium/
-sudo chown -R www-data:www-data /opt/observium/rrd && sudo chown -R www-data:www-data /opt/observium/logs && sudo chmod -R 777 /opt/observium/rrd
-sudo cp /opt/observium_pre_update_$date/config.php /opt/observium/
+sudo cp -rp /opt/observium_pre_update_$date/rrd /opt/observium/
+sudo cp -rp /opt/observium_pre_update_$date/logs /opt/observium/
+sudo cp -p /opt/observium_pre_update_$date/config.php /opt/observium/
 echo
 read -p "${bold}If you see no errors, we're now ready to perform the update. Do you wish to continue?${normal}" -n 1 -r
 echo
@@ -39,4 +43,6 @@ then
 fi
 /opt/observium/discovery.php -h all && /opt/observium/poller.php -h all
 sudo rm observium-community-latest.tar.gz
+echo
+echo "All done. Remember to delete the backup folder once you've confirmed everything is ok but keep the backup tarball just in case."
 echo
